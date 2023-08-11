1fe20644034fe6b49d55586fd4159e26f40f19a4
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-08-11 15:38:56 +0100

    event modifier

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
-    // ++
-    export let showModal: boolean = false;
-    // --------------------------------
+    // once - makes sure the event can only be fired once (removes handler)
+    // preventDefault - prevents the default behavior of the event (run e.preventDefault() in the handler)
+    // self - only fire if the e.target is the element itself (e.g. not from a child element)
+    // stopPropagation - prevents the event from bubbling up the DOM tree (run e.stopPropagation() in the handler)
+    // passive - tells the browser that the handler will never call preventDefault() (only works with touch and wheel events)
+    // capture - fires the handler during the capture phase instead of the bubbling phase
 
+
+
+    export let showModal: boolean = false;
     export let message: string = "default message";
     export let isPromo: boolean = false;
 </script>
 
 {#if showModal}
-    <div class="backdrop" class:promo={isPromo} on:click>
+    <div class="backdrop" class:promo={isPromo} on:click|self>
         <div class="modal">
             <p>{message}</p>
         </div>
