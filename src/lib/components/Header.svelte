e753178d8d4c9c40a71ab4f4a15aba87fdb3a82a
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-08-12 14:15:16 +0100

    header and footer

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,22 @@
+<header>
+    <h1>
+        <img
+            src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg"
+            alt="Svelte logo"
+        />
+    </h1>
+</header>
+
+<style>
+    header {
+        background-color: #f7f7f7;
+        padding: 20px;
+    }
+    h1 {
+        margin: 0;
+        text-align: center;
+    }
+    img {
+        max-width: 100px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+    import Header from '$lib/components/Header.svelte';
+    import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<main>ola</main>
+<Header />
+<main>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis repudiandae aliquam ex illum. Eligendi voluptatum ut, rem assumenda officia, excepturi omnis maxime aliquam hic possimus enim itaque quod culpa eum?</p>
+</main>
+<Footer />
 
 <style>
+    main {
+        max-width: 960px;
+        margin: 40px auto;
+    }
 </style>
