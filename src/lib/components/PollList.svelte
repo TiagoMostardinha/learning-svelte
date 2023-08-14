73f0c6b20b4ccbed8b378958ccf29824229f2ef2
Tiago Mostardinha <tiago.mostardinha@ua.pt>
2023-08-14 13:03:55 +0100

    Transitions and Animations

--- src/lib/components/PollList.svelte
+++ src/lib/components/PollList.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { fade, slide, scale } from "svelte/transition";
+    import {flip} from "svelte/animate";
     import type { Poll } from "$lib/types/Poll";
     import PollDetails from "$lib/components/PollDetails.svelte";
     import PollStore from "../../data/PollStore";
@@ -6,7 +8,9 @@
 
 <div class="poll-list">
     {#each $PollStore as poll (poll.id)}
-        <PollDetails {poll} />
+        <div in:fade out:scale|local animate:flip={{duration: 500}}>
+            <PollDetails {poll} />
+        </div>
     {/each}
 </div>
 
